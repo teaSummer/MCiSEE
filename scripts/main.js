db795b06bb7ceab2cf2b2dde32e1f2456d716e2c
teaSummer <see_tea@outlook.com>
2024-04-04 23:17:02 +0800

    【预览版】修改脚本文件

--- scripts/main.js
+++ scripts/main.js
@@ -30,20 +30,21 @@ const launcherChanged = (() => {
             if ($('#proxy').is(':checked') && String(URL).startsWith('https://github.com/')) {
                 URL = 'https://mirror.ghproxy.com/' + URL;
             }
+            const downloadSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M320 336h76c55 0 100-21.21 100-75.6s-53-73.47-96-75.6C391.11 99.74 329 48 256 48c-69 0-113.44 45.79-128 91.2-60 5.7-112 35.88-112 98.4S70 336 136 336h56M192 400.1l64 63.9 64-63.9M256 224v224.03" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="56"></path></svg>';
             if (attribute == 'data-download') {
                 const version = $(target).attr('data-version');
-                button.html('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M320 336h76c55 0 100-21.21 100-75.6s-53-73.47-96-75.6C391.11 99.74 329 48 256 48c-69 0-113.44 45.79-128 91.2-60 5.7-112 35.88-112 98.4S70 336 136 336h56M192 400.1l64 63.9 64-63.9M256 224v224.03" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="56"></path></svg>下载最新稳定版');
+                button.html(downloadSVG + '下载最新稳定版');
                 button.attr('title', '下载尽可能新的稳定正式版');
                 if (version != 'latest') {
-                    button.html('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M320 336h76c55 0 100-21.21 100-75.6s-53-73.47-96-75.6C391.11 99.74 329 48 256 48c-69 0-113.44 45.79-128 91.2-60 5.7-112 35.88-112 98.4S70 336 136 336h56M192 400.1l64 63.9 64-63.9M256 224v224.03" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="56"></path></svg>下载稳定版 ' + version);
+                    button.html(downloadSVG + '下载稳定版 ' + version);
                     button.attr('title', '下载稳定正式版 ' + version);
                 }
             } else {
                 const devVersion = $(target).attr('data-dev-version');
-                button.html('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M320 336h76c55 0 100-21.21 100-75.6s-53-73.47-96-75.6C391.11 99.74 329 48 256 48c-69 0-113.44 45.79-128 91.2-60 5.7-112 35.88-112 98.4S70 336 136 336h56M192 400.1l64 63.9 64-63.9M256 224v224.03" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="56"></path></svg>下载最新开发版');
+                button.html(downloadSVG + '下载最新开发版');
                 button.attr('title', '下载尽可能新的开发测试版');
                 if (devVersion != 'latest') {
-                    button.html('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M320 336h76c55 0 100-21.21 100-75.6s-53-73.47-96-75.6C391.11 99.74 329 48 256 48c-69 0-113.44 45.79-128 91.2-60 5.7-112 35.88-112 98.4S70 336 136 336h56M192 400.1l64 63.9 64-63.9M256 224v224.03" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="56"></path></svg>下载开发版 ' + devVersion);
+                    button.html(downloadSVG + '下载开发版 ' + devVersion);
                     button.attr('title', '下载开发测试版 ' + devVersion);
                 }
             }
