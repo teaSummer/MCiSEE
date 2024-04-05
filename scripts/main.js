cf01c8819370a02176723a546ed47ef1450d5965
teaSummer <see_tea@outlook.com>
2024-04-05 17:19:50 +0800

    【预览版】应用启动器完整名称

--- scripts/main.js
+++ scripts/main.js
@@ -24,10 +24,15 @@ const deviceChanged = (() => {
 $('#device').change(deviceChanged);
 
 const launcherChanged = (() => {
+    const target = $( $(event.target)[0][$(event.target)[0].selectedIndex] );
+    const dataTitle = target.attr('data-title');
+    $('#launcher-title').text('');
+    if (dataTitle && dataTitle != target.val()) {
+        $('#launcher-title').text(`（${dataTitle}）`);
+    }
     for (const attribute of ['data-download', 'data-dev-download', 'data-url']) {
-        const target = $(event.target)[0][$(event.target)[0].selectedIndex];
         const button = $(`.${attribute}-launcher`);
-        let URL = $(target).attr(attribute);
+        let URL = target.attr(attribute);
         if (attribute.endsWith('download')) {
             if ($('#proxy').is(':checked') && String(URL).startsWith('https://github.com/')) {
                 URL = 'https://mirror.ghproxy.com/' + URL;
@@ -41,7 +46,7 @@ const launcherChanged = (() => {
                 }
             });
             if (attribute == 'data-download') {
-                const version = $(target).attr('data-version');
+                const version = target.attr('data-version');
                 button.html(downloadSVG + '下载最新稳定版');
                 button.attr('title', '下载尽可能新的稳定正式版');
                 if (version != 'latest') {
@@ -50,7 +55,7 @@ const launcherChanged = (() => {
                 }
                 emptyrm(version);
             } else {
-                const devVersion = $(target).attr('data-dev-version');
+                const devVersion = target.attr('data-dev-version');
                 button.html(downloadSVG + '下载最新开发版');
                 button.attr('title', '下载尽可能新的开发测试版');
                 if (devVersion != 'latest') {
@@ -71,12 +76,12 @@ $('.launcher').change(launcherChanged);
 
 const proxyChanged = (() => {
     if ($('#proxy').is(':checked')) {
-        $('.launcher-download>a.button').each((index, element) => {
+        $('#launcher-download>a.button').each((index, element) => {
             const link = $(element).attr('href');
             if (link.startsWith('https://github.com/')) $(element).attr('href', 'https://mirror.ghproxy.com/' + link);
         });
     } else {
-        $('.launcher-download>a.button').each((index, element) => {
+        $('#launcher-download>a.button').each((index, element) => {
             $(element).attr('href', $(element).attr('href').replace(/^https:\/\/mirror\.ghproxy\.com\//, ''));
         });
     }
