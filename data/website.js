42a37d91ed21d2cc7b64c02790d95def3e93fbbc
teaSummer <see_tea@outlook.com>
2024-05-18 17:39:15 +0800

    祝MC十五周年快乐！

--- data/website.js
+++ data/website.js
@@ -14,7 +14,7 @@ const utilityWebsite = [
         }
     },
     {
-        "祝MC十五周年快乐！": {
+        "祝MC十五周年快乐！[open]": {
             "15周年庆典": "https://www.minecraft.net/zh-hans/15th-anniversary",
             "领取苦力怕披风": "https://www.minecraft.net/zh-hans/15th-anniversary?mcRef=redeemCape",
             "去Xbox购买正版": "https://www.xbox.com/zh-CN/games/store/minecraft-java-bedrock-edition-for-pc/9NXP44L49SHJ/0010"
