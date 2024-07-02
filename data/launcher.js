453a9df8f159d4d6b183c8d9da498c711d3ac9a5
teaSummer <see_tea@outlook.com>
2024-07-02 16:23:12 +0000

    modified: update XMCL
    [Auto] Update Launcher Version:
      XMCL(stable): 0.44.10 => 0.44.11
    (Time: 2024-07-02 16:23:12 UTC)

--- data/launcher.js
+++ data/launcher.js
@@ -85,8 +85,8 @@ const WindowsLauncher = [
     {
         title: "X Minecraft Launcher",
         abbr: "XMCL",
-        download: "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.44.10/xmcl-0.44.10-win32-x64.zip",
-        version: "0.44.10",
+        download: "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.44.11/xmcl-0.44.11-win32-x64.zip",
+        version: "0.44.11",
         url: "https://github.com/voxelum/x-minecraft-launcher"
     },
     {
@@ -169,8 +169,8 @@ const macOSLauncher = [
     {
         title: "X Minecraft Launcher",
         abbr: "XMCL",
-        download: "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.44.10/xmcl-0.44.10.dmg",
-        version: "0.44.10",
+        download: "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.44.11/xmcl-0.44.11.dmg",
+        version: "0.44.11",
         url: "https://github.com/Voxelum/x-minecraft-launcher"
     },
     {
@@ -226,8 +226,8 @@ const LinuxLauncher = [
     {
         title: "X Minecraft Launcher",
         abbr: "XMCL",
-        download: "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.44.10/xmcl-0.44.10-arm64.deb",
-        version: "0.44.10",
+        download: "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.44.11/xmcl-0.44.11-arm64.deb",
+        version: "0.44.11",
         url: "https://github.com/voxelum/x-minecraft-launcher"
     },
     {
