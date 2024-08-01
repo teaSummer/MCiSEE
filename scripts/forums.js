8204961bf34df2144044cb6b57ace9531e418303
teaSummer <see_tea@outlook.com>
2024-08-02 03:22:10 +0800

    perf: update forums.js

--- scripts/forums.js
+++ scripts/forums.js
@@ -258,12 +258,13 @@ const db_forums = [
     {
         title: "MineTalk",
         url: "https://www.minebox.store",
-        state: "up",
+        state: "close",
         createdAt: "2022/06/06",
-        updatedAt: "2024/04/20",
+        closedAt: "2024/07/28",
+        updatedAt: "2024/07/28",
         hasICP: "yes",
         hasNetSec: "no",
-        note: "Java版与基岩版论坛。",
+        note: "Java版与基岩版论坛。因经济因素影响现已关闭。",
         reference: [
             {
                 title: "闹剧之后:实锤“MineTalk”论坛抄袭＆贩卖用户数据",
@@ -400,7 +401,7 @@ const db_forums = [
         hasICP: "no",
         hasNetSec: "no",
         note: "非大陆服务器。",
-        reference: []
+        reference: [] 
     },
     {
         title: "PiboPibo论坛",
@@ -470,7 +471,7 @@ const db_forums = [
         url: "https://www.ikunmc.com",
         state: "up",
         createdAt: "2023/10/28",
-        updatedAt: "2024/04/20",
+        updatedAt: "2024/07/26",
         hasICP: "no",
         hasNetSec: "no",
         note: "非大陆服务器。",
@@ -484,7 +485,7 @@ const db_forums = [
         updatedAt: "2024/05/24",
         hasICP: "no",
         hasNetSec: "no",
-        note: "非大陆服务器，访问极其缓慢。",
+        note: "非大陆服务器。",
         reference: []
     },
     {
@@ -787,9 +788,9 @@ const db_forums = [
     {
         title: "唯安侠",
         url: "https://weianxq.com",
-        state: "up",
+        state: "down",
         createdAt: "2024/03/03",
-        updatedAt: "2024/05/24",
+        updatedAt: "2024/07/22",
         hasICP: "yes",
         hasNetSec: "yes",
         note: "",
@@ -953,9 +954,9 @@ const db_forums = [
     {
         title: "土豆社区",
         url: "https://bbs2.potatokj.top",
-        state: "failure",
+        state: "down",
         createdAt: "2024/03/24",
-        updatedAt: "2024/06/05",
+        updatedAt: "2024/07/22",
         hasICP: "no",
         hasNetSec: "no",
         note: "非大陆服务器。大部分地区无法访问。",
@@ -997,9 +998,9 @@ const db_forums = [
     {
         title: "iMC",
         url: "https://www.imc.lol/",
-        state: "up",
+        state: "down",
         createdAt: "2024/04/30",
-        updatedAt: "2024/06/30",
+        updatedAt: "2024/07/22",
         hasICP: "no",
         hasNetSec: "no",
         note: "非大陆服务器。",
@@ -1008,9 +1009,9 @@ const db_forums = [
     {
         title: "mcwuebbs",
         url: "https://mcbbs.wuenuo.com",
-        state: "up",
+        state: "down",
         createdAt: "2024/05/15",
-        updatedAt: "2024/06/26",
+        updatedAt: "2024/07/22",
         hasICP: "yes",
         hasNetSec: "yes",
         note: "",
@@ -1022,7 +1023,7 @@ const db_forums = [
         ]
     },
     {
-        title: "axxdsn",
+        title: "Axxdsn论坛",
         url: "https://www.axxdsn.cn/",
         state: "up",
         createdAt: "2024/06/08",
