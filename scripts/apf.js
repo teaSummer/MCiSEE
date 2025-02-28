b35a1188476ad049edd0067662a880048104608c
teaSummer <see_tea@outlook.com>
2025-03-01 00:30:51 +0800

    fix: optimize languages

--- scripts/apf.js
+++ scripts/apf.js
@@ -1,18 +1,22 @@
 const addClass = (target = new Element()) => {
-	target.classList.add("apf");
+    target.classList.add('apf');
 }
-const style = "<style>.apf:hover {transform: none;} body {transform: none;}</style>",
-	  giveUpBtn = "<style>main, footer {transform: rotate(180deg);}</style><a id=\"gub\" class=\"button download\" herf=\"#\" onclick=\"apf.giveUp();\" style=\"position: fixed; bottom: 0; right: 0;\">你干嘛~</a>"
+const style = '<style id="neverapf">.apf:hover {transform: none;} main, footer {transform: none;} body {writing-mode: horizontal-tb;;}</style>',
+giveUpBtn = '<a id="gub" class="button download" herf="#" onclick="apf.giveUp();" style="position: fixed; bottom: 0; right: 0;">你干嘛~</a>'
 const main = (eles = document.querySelectorAll(".page-content *")) => {
-	if(checkDate() || cfg.testMode) eles.forEach(ele => addClass(ele)),
-	document.body.appendChild(document.createRange().createContextualFragment(giveUpBtn));
+    if (isapf) {
+        eles.forEach(ele => addClass(ele));
+        $("body").append('<mdui-snackbar placement="bottom" class="aiyoo" closeable>哎哟~~</mdui-snackbar>')
+        document.body.appendChild(document.createRange().createContextualFragment(giveUpBtn));
+    }
 }
 const giveUp = () => {
-	document.querySelector("[al=\"startDownloading\"]").innerText = "哎哟~~";
-	document.querySelector("html").appendChild(document.createRange().createContextualFragment(style));
-	document.body.removeChild(document.querySelector("#gub"));
+    $('.aiyoo').attr('open', true);
+    $('#gub').remove();
+    document.querySelector('html').appendChild(document.createRange().createContextualFragment(style));
     checkDate = () => false;
-	setTimeout(i18n, 5500);
+    isapf = false;
+    setTimeout(i18n);
 }
 
 export {main, giveUp}
--- scripts/init.js
+++ scripts/init.js
@@ -21,26 +21,29 @@ const downloadClick = (() => $('.download').click(function() {
 const faviconGH = fIconGet('github.com');
 
 // 国际化 (internationalization)
-al.setLangPropPath('locales');
+al.setLangPropPath('locales/%%.yml');
 al.setDefaultCountry({
     en: 'en-US',
     zh: 'zh-CN',
 });
+const languages = [
+    // Crowdin
+    'en-UD',
+    'en-US',
+    'lzh',
+    'zh-CN',
+    'zh-HK',
+    'zh-TW',
+];
+al.current(languages, {url: true, yaml: true})
 const i18n = ((callback = () => {}) => {
-    al.setLangProp([
-        'zh-TW.yml',
-        'zh-HK.yml',
-        'zh-CN.yml',
-        'lzh.yml',
-        'en-US.yml',
-        'en-UD.yml',
-    ], () => {
-        if ((checkDate() || cfg.testMode)) {
-            al.load(navigator.language.startsWith('zh') ? 'lzh' : 'en-UD', al.mode.HTML, callback);
-            return;
-        }
-        al.load(void 0, al.mode.HTML, callback);
-    }, {url: true, yaml: true});
+    if ((checkDate() || cfg.testMode)) {
+        al.setLangProp(['en-UD', 'lzh'], () => {
+            al.load(al.mode.HTML, callback, navigator.language.startsWith('zh') ? 'lzh' : 'en-UD');
+        }, {url: true, yaml: true});
+        return;
+    }
+    al.load(al.mode.HTML, callback);
 });
 
 
