063cdc9c3615c0d7da0f3ae4e04eda81bd76a9d7
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2026-01-01 17:48:18 +0800

    chore: create path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import type { UserConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-export default <UserConfig>{
+export default<UserConfig> {
 	resolve: {
 		alias: {
-			'@': '/src',
-			'@views': '/src/views',
-			'@store': '/src/store',
+			'@': path.resolve(__dirname, 'src'),
+			'@comps': path.resolve(__dirname, 'src/components'),
+			'@views': path.resolve(__dirname, 'src/views'),
+			'@store': path.resolve(__dirname, 'src/store'),
+			'@utils': path.resolve(__dirname, 'src/utils'),
 		},
 	},
 	plugins: [vue()],
+	build: {
+		modulePreload: true,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vue: ['vue'],
+					'vue-router': ['vue-router'],
+					jsonc: ['jsonc-parser'],
+				}
+			}
+		}
+	}
 };
