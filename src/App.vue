6132887fd5b067f06f370cc8751f2e6b1de5ff76
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2026-01-01 16:55:06 +0800

    feat(app): add nav

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
 import { useI18nStore } from './store/i18n';
-const i18nStore = useI18nStore();
-const $t = i18nStore.t;
+const { t: $t } = useI18nStore();
+const router = useRouter();
 </script>
 
 <template>
-	<router-view></router-view>
+	<nav>
+		<div class="logo-panel">
+			<router-link to="/">
+				<span class="title" v-html="$t('introduction.title')"></span>
+			</router-link>
+		</div>
+		<search-panel />
+	</nav>
+	<router-view />
 	<footer>
 		<p v-html="$t('ICP')"></p>
 		<p>{{ $t('credit') }}</p>
